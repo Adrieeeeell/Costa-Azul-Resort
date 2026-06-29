b55f6403e8822edaec4270f616b2c3ec8d090ba6
Mariana M <mejiagonzalezmariana007@gmail.com>
2026-06-29 14:44:35 -0600

    Se crea la clase Enum para clientes
    Clase enum para clientes

---
+++ src/Clientes/Enum.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Clientes;
+
+/**
+ *
+ * @author Student
+ */
+public enum Enum {
+    Nacional,Internacional;
+}
